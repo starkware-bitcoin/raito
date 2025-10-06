6e5daef8ab573521492c0598aaef4d9ace9474ec
Maciej Kamiński <aundumla@gmail.com>
2025-10-06 15:08:31 +0200

    Format

--- raito-spv-verify-sdk/scripts/strip-bad-wasm-dts.js
+++ raito-spv-verify-sdk/scripts/strip-bad-wasm-dts.js
@@ -1,11 +1,14 @@
-import fs from "fs";
+import fs from 'fs';
 
 const paths = [
-  "dist/bundler/index_bg.wasm.d.ts",
-  "dist/node/index_bg.wasm.d.ts"
+  'dist/bundler/index_bg.wasm.d.ts',
+  'dist/node/index_bg.wasm.d.ts',
 ];
 for (const p of paths) {
-  let s = fs.readFileSync(p, "utf8");
-  s = s.split("\n").filter(l => !l.includes("LIBBZ2_RS_SYS_v0.1.x_")).join("\n");
+  let s = fs.readFileSync(p, 'utf8');
+  s = s
+    .split('\n')
+    .filter(l => !l.includes('LIBBZ2_RS_SYS_v0.1.x_'))
+    .join('\n');
   fs.writeFileSync(p, s);
 }
